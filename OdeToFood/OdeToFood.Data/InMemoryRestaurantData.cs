74db821e0cdf03590d4dc2e9355f7ec008cb3eb7
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2019-10-25 08:35:28 -0400

    Module 2: Drilling into Data

---
+++ OdeToFood/OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>() 
+            {
+                new Restaurant {Id= 1, Name="Adonis", Location="Etobicoke", Cuisine = CuisineType.Italian},
+                new Restaurant {Id= 2, Name="Karahi", Location="York", Cuisine = CuisineType.Mexican},
+                new Restaurant {Id= 3, Name="Masrawy", Location="Mississauga", Cuisine = CuisineType.Indian}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private IConfiguration config;
+        public readonly IRestaurantData restaurantData;
+        public string message { get; set; }
+        public IEnumerable<Restaurant> Restaurants;
+        
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet()
+        {
+            this.message = config["Message"];
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
